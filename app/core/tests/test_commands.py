e480507c8c2dd1f6690b319e8e377c2ed39bc99d
Siddharth Murjani <SiddharthM1@mobiquityinc.com>
2022-09-02 18:25:52 +0530

    Implement manage user API

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,5 +1,5 @@
 from unittest.mock import patch
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
@@ -13,9 +13,9 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_checked):
-        patched_checked.side_effect = [Psycopg2Error] * 2 + \
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
                                       [OperationalError] * 3 + [True]
         call_command('wait_for_db')
-        self.assertEqual(patched_checked.call_count, 6)
-        patched_checked.assert_called_with(databases=['default'])
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
